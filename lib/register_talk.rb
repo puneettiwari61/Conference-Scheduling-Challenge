32ce667b436555b4f9682be9c8ef1bdb25e334ff
Puneet <puneettiwari61@gmail.com>
2021-01-06 02:50:37 +0530

    add register_talk class with basic functionality

---
+++ lib/register_talk.rb
@@ -0,0 +1,130 @@
+require_relative './talk_list'
+
+
+class RegisterTalk
+
+    include TalkList
+
+    def initialize
+        @updated_list = TALK_LIST
+        @track1 = []
+        @track2 = []
+        register_track_one_before_lunch
+        register_track_two_before_lunch
+        register_track_one_after_lunch
+        register_track_two_after_lunch
+        puts "\n#{@track1}"
+        puts "\n#{@track2}"
+
+    end
+
+    def register_track_one_before_lunch
+        day, month, year = Time.now.day, Time.now.month, Time.now.year
+        startTime = Time.local(year, month, day, 9)
+        available_minutes_before_lunch = 180
+        # puts @updated_list
+        @updated_list.map.with_index do |x,i|
+        if i == 0 
+            x[:startTime] = startTime
+        else
+            x[:startTime] = TALK_LIST[i-1][:endTime]
+        end 
+        x[:endTime] =  x[:startTime] + (x[:duration] * 60)
+        available_minutes_before_lunch = available_minutes_before_lunch - x[:duration]
+        break if available_minutes_before_lunch < 0
+        # puts available_minutes_before_lunch
+        @track1 << x 
+        @updated_list[i][:registered?] = true
+        # print TALK_LIST
+        end
+        # @track1.map {|x| puts x}
+        # puts @updated_list
+    end
+
+    def register_track_two_before_lunch
+        day, month, year = Time.now.day, Time.now.month, Time.now.year
+        startTime = Time.local(year, month, day, 9)
+        available_minutes_before_lunch = 180
+        # unregistered_list = @updated_list.reject {|x|  x[:registered?] || x[:duration] == 5}
+        # puts unregistered_list
+        @updated_list.map.with_index do |x,i|
+            # puts x
+            # next if x[:duration] == 5
+            # puts "hello #{i}"
+        if x[:registered?] == false
+            # if i == 0 
+            # else
+            # x[:startTime] = @updated_list[i-1][:endTime]
+            # end 
+            available_minutes_before_lunch = available_minutes_before_lunch - x[:duration]
+            break if available_minutes_before_lunch < 0
+            x[:startTime] = startTime
+            x[:endTime] =  x[:startTime] + (x[:duration] * 60)
+            startTime = x[:endTime]
+            # puts available_minutes_before_lunch
+            @track2 << x 
+            @updated_list[i][:registered?] = true
+            # print @track2 
+        end
+    end
+    # @track2.map {|x| puts x}   
+    # puts @updated_list
+
+    end
+
+    def register_track_one_after_lunch
+        day, month, year = Time.now.day, Time.now.month, Time.now.year
+        startTime = Time.local(year, month, day, 1)
+        available_minutes_after_lunch = 240
+        # puts @updated_list
+        @updated_list.map.with_index do |x,i|
+            if x[:registered?] == false
+
+                available_minutes_after_lunch = available_minutes_after_lunch - x[:duration]
+                break if available_minutes_after_lunch < 0
+
+                x[:startTime] = startTime
+                x[:endTime] =  x[:startTime] + (x[:duration] * 60)
+                startTime = x[:endTime]
+
+                # puts available_minutes_after_lunch
+                @track1 << x 
+                @updated_list[i][:registered?] = true
+                # print TALK_LIST
+                end
+            end
+        # @track1.map {|x| puts x}
+        # puts @updated_list
+    end
+
+    def register_track_two_after_lunch
+        day, month, year = Time.now.day, Time.now.month, Time.now.year
+        startTime = Time.local(year, month, day, 1)
+        available_minutes_after_lunch = 240
+        # unregistered_list = @updated_list.reject {|x|  x[:registered?] || x[:duration] == 5}
+        # puts unregistered_list
+
+        @updated_list.map.with_index do |x,i|
+            if x[:registered?] == false
+            # puts x
+            # next if x[:duration] == 5
+            # puts "hello #{i}"
+            available_minutes_after_lunch = available_minutes_after_lunch - x[:duration]
+            break if available_minutes_after_lunch < 0
+            x[:startTime] = startTime
+        x[:endTime] =  x[:startTime] + (x[:duration] * 60)
+        startTime = x[:endTime]
+        # puts available_minutes_before_lunch
+        @track2 << x 
+        # print @track1
+        @updated_list[i][:registered?] = true
+        end
+    end
+        # @track2.map {|x| puts x}   
+        # puts @updated_list
+    end
+
+end
+
+talk = RegisterTalk.new
+
---
+++ lib/talk_list.rb
@@ -0,0 +1,27 @@
+module TalkList
+
+ 
+    TALK_LIST=[   
+        {name: "Ansible : An alternative to chef",registered?: false, duration: 5},
+        {name: "Riding Rails for 10 years",registered?: false, duration: 5},
+        {name: "Pryin open the black box",registered?: false, duration: 60},
+        {name: "Migrating a huge production codebase from sinatra to Rails",registered?: false, duration: 45},
+        {name: "How does bundler work",registered?: false, duration: 30},
+        {name: "Ruby on Rails on Minitest",registered?: false, duration: 30},
+        {name: "Sustainable Open Source",registered?: false, duration: 45},
+        {name: "How to program with Accessiblity in Mind",registered?: false, duration: 45},
+        {name: "Implementing a strong code review culture",registered?: false, duration: 60},
+        {name: "Scaling Rails for Black Friday",registered?: false, duration: 45},
+        {name: "Docker isn't just for deployment",registered?: false, duration: 30},
+        {name: "Callbacks in Rails",registered?: false, duration: 30},
+        {name: "Microservices, a bittersweet symphony",registered?: false, duration: 45},
+        {name: "Teaching github for poets",registered?: false, duration: 60},
+        {name: "Heroku: A year in review",registered?: false, duration: 30},
+        {name: "Test Driving your Rails Infrastucture with Chef",registered?: false, duration: 60},
+        {name: "SVG charts and graphics with Ruby",registered?: false, duration: 45},
+        {name: "Interviewing like a unicorn: How Great Teams Hire",registered?: false, duration: 30},
+        {name: "How to talk to humans: a different approach to soft skills",registered?: false, duration: 30},
+        {name: "Getting a handle on Legacy Code",registered?: false, duration: 60}
+    ]
+    
+end
