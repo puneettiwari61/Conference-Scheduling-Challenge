75929c781e61bcaa3802722bec67a1a76fa671eb
Puneet <puneettiwari61@gmail.com>
2021-01-06 13:18:24 +0530

    code refactor

--- lib/register_talk.rb
+++ lib/register_talk.rb
@@ -7,60 +7,58 @@ class RegisterTalk
     include TalkList
 
     def initialize
-        @updated_list = TALK_LIST
+        @talk_list = TALK_LIST
         @track1 = []
         @track2 = []
-        register_track_one_before_lunch
-        register_track_two_before_lunch
-        register_track_one_after_lunch
-        register_track_two_after_lunch
-        print_talks
     end
 
-    def register_track_one_before_lunch
+    def register_talk_sessions
+
+        register_track_one_morning_session
+        register_track_two_morning_session
+        register_track_one_afternoon_session
+        register_track_two_afternoon_session
+
+    end
+
+
+    def register_track_one_morning_session
     
-    session = Session.new(@track1, @updated_list, "morning")
-
-    @track1, @updated_list = session.create_track
-
-    end
-
-    def register_track_two_before_lunch
-
-
-    session = Session.new(@track2, @updated_list, "morning")
-
-
-    @track2, @updated_list = session.create_track
+        session = Session.new(@track1, @talk_list, "morning")
+        @track1, @talk_list = session.create_track_and_update_list
 
     end
 
-    def register_track_one_after_lunch
+    def register_track_two_morning_session
 
-        session = Session.new(@track1, @updated_list, "afternoon")
-
-
-        @track1, @updated_list = session.create_track
+        session = Session.new(@track2, @talk_list, "morning")
+        @track2, @talk_list = session.create_track_and_update_list
 
     end
 
-    def register_track_two_after_lunch
+    def register_track_one_afternoon_session
 
-    session = Session.new(@track2, @updated_list, "afternoon")
-
-    @track2, @updated_list = session.create_track
+        session = Session.new(@track1, @talk_list, "afternoon")
+        @track1, @talk_list = session.create_track_and_update_list
 
     end
 
-    def print_talks
+    def register_track_two_afternoon_session
+
+        session = Session.new(@track2, @talk_list, "afternoon")
+        @track2, @talk_list = session.create_track_and_update_list
+
+    end
+
+    def print_schedule
         puts "\nTrack 1"
         @track1.map {|x| x.to_s }
         puts "\nTrack2"
         @track2.each {|x| x.to_s}
-
     end
 
 end
 
-talk = RegisterTalk.new
+
+
 
